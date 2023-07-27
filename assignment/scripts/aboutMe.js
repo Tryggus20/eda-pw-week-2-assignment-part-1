bf94e96e74a8640ccf1e80e26bccc5b14aa6a4d4
David Campbell <tryggus20@gmail.com>
2023-07-26 20:49:43 -0500

    fixed adventurous false

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,32 +7,32 @@ let lastName = ' Campbell';
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + lastName;
 // 4 - Console log the value of `fullName`
-console.log( fullName )
+console.log( fullName );
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = 2
+let luckyNumber = 19;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 console.log ('My name is' , fullName , 'and I think' , luckyNumber , 'is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = true
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-let food = 'ribs'
+let food = 'ribs';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets = 0
+let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 0
+let friendsPets = 0;
 // 11 - Add two pets to your `pets` variable
-pets = 2
+pets = 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-let allowedPets = 4
+let allowedPets = 4;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 if( adventurous == true ){
-    console.log( "Adventures are great!")
+    console.log( "Adventures are great!");
 }
-else{
-    "How about we stay home?"
+else {
+    console.log("How about we stay home?");
 }
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
