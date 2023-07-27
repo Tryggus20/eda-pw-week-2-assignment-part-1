f05e4f08e06a2650e753545068428f64dff34b37
David Campbell <tryggus20@gmail.com>
2023-07-26 19:50:38 -0500

    completed pet comparison stretch

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -21,9 +21,9 @@ let food = 'ribs'
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 0
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 1
+let friendsPets = 0
 // 11 - Add two pets to your `pets` variable
-pets = 0
+pets = 2
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 let allowedPets = 4
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
@@ -55,6 +55,7 @@ if( pets > allowedPets ){
    console.log("Oh no, I have too many pets!")
 }
 
+
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -62,7 +63,19 @@ if( pets > allowedPets ){
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-
+let mostPets = 0
+if( pets > friendsPets ){
+    mostPets = pets
+    console.log("I have more pets than my friend")
+}
+else if ( pets == friendsPets){
+    mostPets = pets
+    console.log( "My friend and I have the same amount of pets" )
+}
+else{
+    mostPets = friendsPets
+    console.log( "My friend has more pets than me")
+}
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
